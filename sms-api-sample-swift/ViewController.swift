1c51b76852f4ae49c7e22676fb4044db30c98b08
robertkadikilo <robert@barriowatch.com>
2020-09-24 11:49:26 +0300

    Initial Commit

---
+++ sms-api-sample-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sms-api-sample-swift
+//
+//  Created by Robert Kadikilo on 24/09/2020.
+//  Copyright © 2020 Full Stack Team Six. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
